16df75e6ec30bf1ccbe3a367478a9cd13f7f5a04
Jacob Pfeiffer <pfeiffer@hartford.edu>
2018-03-29 17:16:11 -0400

    Set up handlebars view engine, started setting up authentication

--- app.js
+++ app.js
@@ -1,43 +1,37 @@
-/*
-// HTTPS setup
-let fs = require('fs');
-let http = require('http');
-let https = require('https');
-let privateKey = fs.readFileSync('certs/server.key', 'utf8'); // TODO: Generate certs
-let certificate = fs.readFileSync('certs/server.crt', 'utf8');
-const credentials = {
-	key: privateKey, 
-	cert: certificate
-};
-*/
-
 let express = require('express');
 let app = express();
 
-app.use(express.static('public'));
+// View engine things
+app.set('view engine', 'html'); // Set the view engine extension
+app.engine('html', require('hbs').__express); // Set the view engine itself
+
+app.get('/', (req, res) => {
+	let exampleObject = {title: "Gargoyle"};
+	res.render('index', exampleObject); // Rendering example
+});
 
 // Routing, index.js doesn't need to be included in the require path because index.js is the default for a directory
-let login = require('./login.js'); // Likely use for processing login
+let login = require('./routes/login.js'); // Likely use for processing login
 app.use('/login', login);
 
 // Other views can follow the format of students/admin
 let students = require('./routes/students'); 
-app.use('/students', students);
+app.use('/students', verify, students); // 
 
 let admin = require('./routes/admin');
-app.use('/admin', admin);
+app.use('/admin', verify, admin);
 
 let help = require('./routes/help');
 app.use('/help', help);
 
-/*
-// HTTPS things
-let httpServer = http.createServer(app);
-let httpsServer = https.createServer(credentials, app);
-httpServer.listen(80);
-httpsServer.listen(443);
-*/
+// Public directory
+app.use(express.static('public'));
 
-app.listen(80, () => console.log("App listening on port 80"));
+app.listen(8080, () => console.log("App listening on port 8080"));
+
+function verify(req, res, next) {
+	// TODO: Middleware to verify auth
+	next();
+}
 
 // TODO: Add passport.js, express-session
