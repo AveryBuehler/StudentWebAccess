c472e58a3786a5746ac6a814f2761b672baf4017
Jacob Pfeiffer <pfeiffer@hartford.edu>
2018-04-18 10:39:12 -0400

    Class list, personal info list, updated links in all pages

--- routes/students/index.js
+++ routes/students/index.js
@@ -1,14 +1,31 @@
 // Student view
 
-const express = require('express');
+let express = require('express');
 let router = express.Router();
+
+let knex = require('../../dbconnection.js').connection(process.argv);
 
 router.route('/')
 	.get(function(req, res) {
 		res.render('index', {});
-	})
-	.post(function(req, res) {
-		res.render('index', {});
+	});
+
+router.route('/personal')
+	.get(function(req, res) {
+		res.render('personal', req.user);
+	});
+
+router.route('/academic')
+	.get(function(req, res) {
+		res.render('academic', req.user);
+	});
+
+router.route('/classes')
+	.get(function(req, res) {
+		knex.select().table('classes.classes').then(function(data) {
+			res.render('classes', {classes: data});
+		});
+		
 	});
 
 module.exports = router;
